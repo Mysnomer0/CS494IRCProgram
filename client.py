0fbf5fbaccafa451c38c79b7d0aecffd1f99d82d
milan.mauck <milan.mauck@pcc.edu>
2020-02-17 21:29:42 -0800

    Using sockets instead WIP

--- client.py
+++ client.py
@@ -1,13 +1,67 @@
-import argparse
+import sys
+import socket
+import select
 
 class Client:
-    def __init__(self, name):
+    def __init__(self, name, host, port):
         self.name = name
+        self.host = host
+        self.port = port
+        self.exit = False
+
+        # Get the socket isntance
+        currentSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        currentSocket.settimeout(2)
+        
+        # Try to connect to the host
+        try:
+            currentSocket.connect((self.host, self.port))
+        except :
+            print('Can\'t connect.')
+            return
+        
+        # Clear the standard out
+        print('Connected!')
+        sys.stdout.write('[Me] ')
+        sys.stdout.flush()
+        print('Starting loop')
+        # While we're connected, listen on the socket
+        while not self.exit:
+            # Get the socket list
+            socketList = [sys.stdin, currentSocket]
+            # Get the list sockets which are readable
+            readyToRead,readyToWrite,inError = select.select(socketList , [], [])
+            # Go through all the sockets
+            for sock in readyToRead:
+                # If we're on the server's socket   
+                if sock == currentSocket:
+                    # Get the data from the received message
+                    data = sock.recv(4096)
+                    # If the data is nonexistent, this means we've been disconnected
+                    if not data:
+                        print('\nDisconnected from server')
+                        self.exit = True
+                        break
+                    # Otherwise, output the received data
+                    else:                    
+                        sys.stdout.write(data)
+                        sys.stdout.write('[Me] ')
+                        sys.stdout.flush()     
+                # Otherwise, read the input
+                else:
+                    # user entered a message
+                    message = sys.stdin.readline()
+                    currentSocket.send(message)
+                    sys.stdout.write('[Me] ')
+                    sys.stdout.flush()
 
 
 if __name__ == '__main__':
+    
     # Parse the arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("command")
-    arguments = parser.parse_args()
+    while True:
+        if input('Would you like to create a new connection? (y/n) ') == 'n':
+            break
+        # Create the client with the new connection
+        client = Client(input('What is your name? '), input('What host would you like to connect to? '), int(input('What port would you like to connect to? ')))
 
--- server.py
+++ server.py
@@ -1,39 +1,93 @@
 # Milan Mauck
 
-from flask import *
 from room import Room
 from client import Client
+import sys
+import socket
+import select
 
+class Server:
+    def __init__(self):
+        self.listOfRooms = []
+        self.listOfClients = []
+        self.host = '' 
+        self.socketList = []
+        self.receiveBuffer = 4096 
+        self.port = 9090
 
-# Create the server as an HTTP app
-app = Flask('CS494IRCServer')
+    def StartServer(self):
+        
+        # Setup the server socket
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        serverSocket.bind((self.host, self.port))
+        serverSocket.listen(10)
+    
+        # Add the server socket object to the list of readable connections
+        self.socketList.append(serverSocket)
+    
+        print("Server started on port " + str(self.port))
+    
+        # Continuously listen for new connections
+        while True:
 
-# The list of rooms
-listOfRooms = []
-# The list of clients
-listOfClients = []
+            # get the list sockets which are ready to be read through select
+            # 4th arg, time_out  = 0 : poll and never block
+            isReadyToRead,isReadyToWrite,inError = select.select(self.socketList,[],[],0)
+        
+            # Check for any new connection requests
+            for sock in isReadyToRead:
+                # If a new connection request recieved
+                if sock == serverSocket: 
+                    # Accept the new connection
+                    socketForward, address = serverSocket.accept()
+                    # Add this socket to the list of connected
+                    self.socketList.append(socketForward)
+                    print('Client with address ' + str(address) + ' connected!')
+                    
+                    self.Broadcast(serverSocket, socketForward, "[%s:%s] entered room\n" % address)
+                
+                # Otherwise, process a message from a client
+                else:
+                    try:
+                        # Get the data from the socket
+                        data = sock.recv(self.receiveBuffer)
+                        # If there actually is data
+                        if data:
+                            # self.Broadcast the message
+                            self.Broadcast(serverSocket, sock, "\r" + '[' + str(sock.getpeername()) + '] ' + data)  
+                        else:
+                            # Otherwise, the connection is broken, so remove the socket from the list
+                            if sock in self.socketList:
+                                self.socketList.remove(sock)
 
-# The list rooms command
-@app.route('/rooms', methods=['POST'])
-def ListRooms():
-    if request.method == 'POST':
-        listOfRoomsAsString = ''
-        for i in range(len(listOfRooms)):
-            listOfRoomsAsString += listOfRooms[i].name
-        return listOfRoomsAsString
+                            # Tell everyone the client disconnected
+                            self.Broadcast(serverSocket, sock, "Client (%s, %s) is offline\n" % address) 
 
-# The list rooms command
-@app.route('/connect', methods=['POST'])
-def ListRooms():
-    if request.method == 'POST':
-        # Add this client to the list
-        listOfClients.append(Client(''))
+                    # Tell everyone the client disconnected
+                    except:
+                        self.Broadcast(serverSocket, sock, "Client (%s, %s) is offline\n" % address)
+                        continue
 
-# A ping function that pings all clients currently connected and removes them if necessary
+        serverSocket.close()
+        
+    # Sends the given message to all connected clients
+    def Broadcast(self, serverSocket, sock, message):
+        for socket in self.socketList:
+            # Make sure we're sending the message to the right socket
+            if socket != serverSocket and socket != sock :
+                # Try to send the message
+                try :
+                    socket.send(message)
+                # If it fails, we have a broken connection
+                except :
+                    socket.close()
+                    if socket in self.socketList:
+                        self.socketList.remove(socket)
+ 
+if __name__ == "__main__":
 
-@app.route('/')
-def hello():
-    return "Hello World!"
-
-if __name__ == '__main__':
-    app.run()
+    # Create a server
+    server = Server()
+    # Start the server listening
+    sys.exit(server.StartServer()) 
